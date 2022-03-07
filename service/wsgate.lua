ec3f866088f5a13a51606b865296a57797c88cee
rossoneri <panwei@7cpoker.com>
2022-03-07 11:13:27 +0800

    meger code t1.0

--- service/wsgate.lua
+++ service/wsgate.lua
@@ -21,13 +21,17 @@ function handler.message(fd, msg, sz)
 	-- recv a package, forward it
 	local c = connection[fd]
 	local agent = c.agent
+	print("agent",agent,fd,inspect(msg),inspect(sz))
 	if agent then
 		skynet.redirect(agent, c.client, "client", fd, msg, sz)
 	else
-		skynet.send(watchdog, "lua", "socket", "data", fd, netpack.tostring(msg, sz))
+		skynet.send(watchdog or "watchdog", "lua", "socket", "data", fd, netpack.tostring(msg, sz))
+		-- print(watchdog, c.client or 0, "client", fd, msg, sz)
+		-- skynet.redirect(watchdog, c.client or 0, "client", fd, msg, sz)
 	end
 end
 
+-- 发送给 watchdog
 function handler.connect(fd, addr)
 	local c = {
 		fd = fd,
@@ -35,6 +39,9 @@ function handler.connect(fd, addr)
 	}
 	connection[fd] = c
 	skynet.send(watchdog, "lua", "socket", "open", fd, addr, "websocket")
+	--gateserver.openclient(fd)
+
+	print(inspect(connection))
 end
 
 local function unforward(c)
@@ -48,19 +55,27 @@ end
 local function close_fd(fd)
 	local c = connection[fd]
 	if c then
+		print("wsgate close_fd", fd)
 		unforward(c)
 		connection[fd] = nil
+		gateserver.closeclient(fd)
+		return true
 	end
 end
 
 function handler.disconnect(fd)
-	close_fd(fd)
-	skynet.send(watchdog, "lua", "socket", "close", fd)
+	print("wsgate disconnect", fd)
+	if close_fd(fd) then
+		skynet.send(watchdog, "lua", "socket", "close", fd)
+		gateserver.closeclient(fd)
+	end
 end
 
+
 function handler.error(fd, msg)
-	close_fd(fd)
-	skynet.send(watchdog, "lua", "socket", "error", fd, msg)
+	if close_fd(fd) then
+		skynet.send(watchdog, "lua", "socket", "error", fd)
+	end
 end
 
 function handler.warning(fd, size)
@@ -69,7 +84,15 @@ end
 
 local CMD = {}
 
+function CMD.watchdog(source, _watchdog)
+	print(source, _watchdog)
+	watchdog = _watchdog
+end
+
+-- 这里的 agent 进房前是 watchdog 进房后是 房间地址
 function CMD.forward(source, fd, client, address)
+	print("wsgate forward",source, fd, client, address)
+	skynet.trace()
 	local c = assert(connection[fd])
 	unforward(c)
 	c.client = client or 0
@@ -85,9 +108,9 @@ function CMD.accept(source, fd)
 end
 
 function CMD.kick(source, fd)
-	gateserver.closeclient(fd)
+	close_fd(fd)
 end
-
+--逻辑 自己调自己
 function handler.command(cmd, source, ...)
 	local f = assert(CMD[cmd])
 	return f(source, ...)
